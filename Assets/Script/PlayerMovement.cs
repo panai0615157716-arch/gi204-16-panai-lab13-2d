ec2a23e4d5d0f166a809a814e3c153b1ddabcb6d
panai0615157716-arch <panai0615157716@gmail.com>
2026-04-24 19:07:38 +0700

    V3

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -2,12 +2,15 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+
+    Vector2 moveInput;
     [SerializeField] float Speed;
     float move;
-
+    
     [SerializeField] float JumpForce;
+    [SerializeField] bool IsJumping;
 
-    Rigidbody2D rb2d;
+    Rigidbody2D rb2d; 
 
     void Start()
     {
@@ -16,17 +19,41 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        move = Input.GetAxis("Horizontal");
+        /*move = Input.GetAxis("Horizontal");
+        rb2d.linearVelocity = new Vector2(move * Speed, rb2d.linearVelocity.y);*/
 
-        rb2d.linearVelocity = new Vector2(move * Speed, rb2d.linearVelocity.y);
+        moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        rb2d.AddForce(moveInput * Speed);
 
-        if (Input.GetButtonDown("Jump")) 
+
+        if (Input.GetButtonDown("Jump") && !IsJumping)
         {
             rb2d.AddForce(new Vector2(rb2d.linearVelocity.x, JumpForce));
-
             Debug.Log("Jump");
         
         }
+    
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        { 
+            IsJumping = false;
+        }
+    }
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground")) 
+        {
+            IsJumping = true; 
+        
+        }
+
 
     }
+
+
+
+
 }
